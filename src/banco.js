423568c9cb7a859b9244fe5d6a400c7f78816a9d
Ricardo Nascimento <ricardodavivi@gmail.com>
2020-10-20 10:29:41 -0300

    first commit

---
+++ src/banco.js
@@ -0,0 +1,5 @@
+var banco = {
+    
+};
+
+exports.db = banco;
---
+++ src/stages/0.js
@@ -0,0 +1,22 @@
+const cardapio = require("../cardapio");
+const banco = require("../banco");
+
+function execute(user, msg, contato) {
+  //let menu = " CARDAPIO \n\n";
+
+  //Object.keys(cardapio.menu).forEach((value) => {
+    //let element = cardapio.menu[value];
+    //menu += `${value} - ${element.descricao}        R$ ${element.preco} \n`;
+  //});
+
+  //banco.db[user].stage = 1;
+
+  return [
+    `Olá ${contato} sou uma assistente virtual e me chamo Brook, 
+    esse é meu novo aplicativo, lhe ajudará a fazer compras com mais 
+    facilidade, é só apertar no link: 'https://meuappdelivery.netlify.app/'`,
+    //menu,
+  ];
+}
+
+exports.execute = execute;
